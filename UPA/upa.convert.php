6e823e085398ab91226c14eb5bacf9bb5a2c17c3
Asfo <asfo@flikes.net>
2011-12-12 00:47:20 -0600

    [22][UPA]Próximo proyecto, realizar una "aplicación" que tenga utilidades que le puedan servir al usuario como "librería" y facilitar el manejo de sus proyectos.

---
+++ UPA/upa.convert.php
@@ -0,0 +1,18 @@
+<?php
+/*
+* UpA
+* Utiidades por Asfo
+* 
+* @Paquete			Utilidades por Asfo
+* @Version			0.1
+* @Autor			Asfo
+* @Licencia			GPL v2
+* @Link				http://github.com/asfo
+* @Copyright		Copyright (C) 2011 Asfo
+*/
+namespace UpA;
+
+class Convert{
+	
+}
+?>
