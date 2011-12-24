92a133fa443b9b2eb2086d8002aa2cb318ec909d
Asfo <asfo@flikes.net>
2011-12-24 03:28:31 -0600

    [52][Framework]Nueva definición: ACTIVE_WEB que emula si la web está o no activa. Así mismo, se acompaña de un mensaje.

--- Framework/www/config/config.basics.php
+++ Framework/www/config/config.basics.php
@@ -10,7 +10,15 @@
  *		----------------------------------------------------
  *
  **********************************************************/
- 
+  	if(!DEFINED('_ACCESS'))
+		die("<h1>Error</h1><p>No tienes permiso para acceder aqu&iacute;</p>");
+	
  //Configuración (por medio de una definición) de la aplicación en uso
  DEFINE('DEFAULT_APPLICATION', 'default');
+ 
+ //Configuración (por medio de una definición) de el estado de la web [TRUE != true] | [TRUE = ACTIVADA | FALSE = DESACTIVADA]
+ DEFINE('ACTIVE_WEB', TRUE);
+ 
+ //Configuración (por medio de una definición) del mensaje a mostrar en caso de que esté en construcción la web/aplicación
+ DEFINE('DISABLED_WEB_MESSAGE', '<h1>En Construcci&oacute;n</h1><p>Esta web se encuentra en construcci&oacute;n, vuelva m&aacute;s tarde</p>');
 ?>
