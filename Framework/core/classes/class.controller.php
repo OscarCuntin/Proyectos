11dc5bfd07ce46650d8ff3f2435bb8595faa40c6
Asfo <asfo@flikes.net>
2011-12-27 17:07:27 -0600

    [68][Framework] Nuevo archivo: class.controller.php inicializado.

--- Framework/core/classes/class.controller.php
+++ Framework/core/classes/class.controller.php
@@ -10,6 +10,20 @@
  *		----------------------------------------------------
  *
  **********************************************************/
+  if(!DEFINED('_ACCESS'))
+	die(Error("Error", "No puedes acceder a este archivo directamente"));
+
+ class AsfoController extends AsfoLoad
+ {
+	
+	public $Helpers;
  
+	public function __construct(){
+		$this -> Helpers = array('alerts', 'debugging', 'time', 'string', 'forms', 'security');
+	}
  
+	public function Helpers(){
+		$this -> Helper($this -> Helpers);
+	}
+ }
  ?>
