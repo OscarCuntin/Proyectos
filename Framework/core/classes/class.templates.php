99b3b011fc99ec8e03da9ea8ef53348c0a534dc0
Asfo <asfo@flikes.net>
2011-12-27 17:08:04 -0600

    [69][Framework]Nuevo archivo: class.templates.php e inicialización del mismo.

---
+++ Framework/core/classes/class.templates.php
@@ -0,0 +1,88 @@
+<?php
+/***********************************************************
+ *	
+ *		Asfo Framework			
+ * 		----------------------------------------------------
+ *		Autor		:	Asfo Zavala
+ *		Copyright	: 	Copyright (C) 2011, Asfo Zavala
+ *		Licencia	:	GNU GPL v3
+ *		Link		: 	http://github.com/Asfo/
+ *		----------------------------------------------------
+ *
+ **********************************************************/
+ 
+	if(!DEFINED('_ACCESS'))
+		die(Error("Error", "No puedes acceder a este archivo directamente"));
+	
+class AsfoTemplates extends AsfoLoad
+{
+	public $CSS = NULL;
+	/*---*/
+	public $Helpers;
+	/*---*/
+	public $JS = NULL;
+	/*---*/
+	private $THEME = NULL;
+	/*---*/
+	private $themePath;
+	/*---*/
+	private $Vars = array();
+	
+	public function __construct()
+	{
+		$this -> Helpers = array('array', 'browser', 'detect', 'debugging', 'forms', 'html', 'scripts', 'validations');
+		$this -> Helper($this -> Helpers);
+		$this -> Config('cache');
+		$this -> Config('templates');
+	}
+	
+	public function CSS($CSS = NULL, $App = NULL, $Print = FALSE, $Name = NULL)
+	{
+	
+	}
+	
+	public function Exists($Template, $View = FALSE)
+	{
+	
+	}
+	
+	public function getThemes($Theme)
+	{
+	
+	}
+	
+	public function Theme($Theme)
+	{
+	
+	}
+	
+	public function JS($JS = NULL, $App, $Print = FALSE, $Name = NULL)
+	{
+	
+	}
+	
+	public function getTitle($ExtraTitle = NULL)
+	{
+		if(is_null($ExtraTitle)
+			return WEB_NAME; //Configuración
+		else
+			return WEB_NAME . '-' . $ExtraTitle; //Configuración - Título Extra
+	}
+	
+	public function isTheme()
+	{
+	
+	}
+	
+	public function Load()
+	{
+	
+	}
+	
+	public function Vars($Vars)
+	{
+		return $this -> Vars = $Vars;
+	}
+
+}
+ ?>
