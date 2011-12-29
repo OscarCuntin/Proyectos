a4025725c9f6de703dc3cbe8fb9a930277004a78
Asfo <asfo@flikes.net>
2011-12-29 15:11:29 -0600

    [79][Framework]Nueva configuración y el driver de mysql actualizado.

--- Framework/core/drivers/driver.mysql.php
+++ Framework/core/drivers/driver.mysql.php
@@ -19,6 +19,10 @@ class AsfoMySQL extends AsfoLoad
 	private static $Connection;
 	/*---*/
 	private $SQL;
+	/*---*/
+	public $InsertQuery;
+	/*---*/
+	public $simpleDeleteQuery;
 	
 	public function __construct()
 	{
@@ -48,6 +52,47 @@ class AsfoMySQL extends AsfoLoad
 		}
 		else
 			return self::$Connection;
+	}
+	
+	public function Query($SQL)
+	{
+		if($SQL != "") //Diferente de vacío
+		{
+			if(stristr($SQL, "call") && stripos($SQL, "call") == 0)
+			{
+				//Con la @ evitamos que nos marque un error, sin embargo, debe hacerse un log para errores como este
+				@mysql_multi_query($SQL, self::$Connection);
+				
+				$this -> Query = @mysql_store_result(self::$Connection);
+				
+				if(@mysql_more_results(self::$Connection))
+					@mysql_next_result(self::$Connection);
+			}
+			else
+				$this -> Query = mysql_query($SQL, self::$Connection);
+		}
+		
+		return ($this -> Query) ? $this -> Query : FALSE;
+	}
+	
+	public function Insert($Table, $Columns, $Values)
+	{
+		if(!$Table || !$Columns || !$Values)
+			return false;
+								//Visual: "INSERT INTO tabla (`algo`) VALUES (1)";
+		$this -> InsertQuery = "INSERT INTO ".$Table." (".$Columns.") VALUES (".$Values.");";
+		
+		return (mysql_query(self::$Connection, $this -> InsertQuery)) ? true : false;
+	}
+	
+	public function simpleDelete($Table, $Identifier, $Where)
+	{
+		if(!$Table || !$Identifier || !$Where)
+			return false;
+									//Visual: "DELETE FROM tabla WHERE algo = 1"
+		$this -> simpleDeleteQuery = "DELETE FROM ".$Table." WHERE ".$Where." = ".$Identifier."";
+		
+		return (mysql_query(self::$Connection, $this -> simpleDeleteQuery)) ? true : false;
 	}
 }
 ?>
---
+++ Framework/www/config/config.templates.php
@@ -0,0 +1,16 @@
+<?php
+/***********************************************************
+ *	
+ *		Asfo Framework			
+ * 		----------------------------------------------------
+ *		Autor		:	Asfo Zavala
+ *		Copyright	: 	Copyright (C) 2011, Asfo Zavala
+ *		Licencia	:	GNU GPL v3
+ *		Link		: 	http://github.com/Asfo/
+ *		----------------------------------------------------
+ *
+ **********************************************************/
+ 
+	if(!DEFINED('_ACCESS'))
+		die("<h1>Error</h1><p>No puedes acceder a este archivo directamente.</p>");
+?>
